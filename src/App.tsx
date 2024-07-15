5213a7b22f2346b6a56c12aac59914b63bf5b873
황주빈 <hjb06288@gmail.com>
2024-07-15 18:53:34 +0900

    test: 구동 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './page/Home';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        {/* 다른 Route들도 필요에 따라 추가 */}
+      </Routes>
+    </Router>
+  );
 }
-export default App
+export default App;
