dc60e93f808b543364b7fee3b80cc1aa48675a0c
황주빈 <hjb06288@gmail.com>
2024-07-16 16:50:00 +0900

    feat: delete, update feature

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ type actionType = 'CREATE' | 'DELETE' | 'UPDATE';
 
 interface Action {
   type: string,
-  data: DataType
+  data: Partial<DataType>
 }
 
 //state: data에 들어있는 그대로
@@ -29,8 +29,17 @@ const reducer = (state: DataType[], action: Action) => {
 
   switch (action.type) {
     case "CREATE": {
-      nextState = [action.data, ...state];
+      nextState = [action.data as DataType, ...state];
       break
+    }
+    case "DELETE": {
+      nextState = state.filter((item) => item.id !== action.data.id)
+      break
+    }
+    case "UPDATE": {
+      nextState = state.map((item) =>
+        item.id === action.data.id ? action.data as DataType : item)
+      break;
     }
     default:
       nextState = state;
@@ -44,6 +53,7 @@ function App() {
   const [data, dispatch] = useReducer(reducer, [] as DataType[]);
   const idRef = useRef(0);
 
+  //새 일기 추가
   const onCreate = (createdDate: string, emotionId: number, content: string) => {
     dispatch({
       type: 'CREATE',
@@ -56,22 +66,42 @@ function App() {
     });
   }
 
+  // 일기 삭제
+  const onDelete = (id: number) => {
+    dispatch({
+      type: "DELETE",
+      data: { id },
+    });
+  };
+
+  //일기 업데이트
+  const onUpdate = (id: number, createdDate: string, emotionId: number, content: string) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id, createdDate, emotionId, content
+      }
+
+    })
+
+  }
+
   const DiaryDataContext = createContext(data);
-  const DiaryDispatchContext = createContext(onCreate);
+  const DiaryDispatchContext = createContext<{ onCreate: typeof onCreate, onDelete: typeof onDelete, onUpdate: typeof onUpdate }>({ onCreate, onDelete, onUpdate });
 
   return (
     <>
       <DiaryDataContext.Provider value={data}>
-        <DiaryDispatchContext.Provider value={onCreate}>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/new" element={<New />} />
-              <Route path="/diary/:id" element={<Diary />} />
-              <Route path="/edit/:id" element={<Edit />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </Router>
+        <DiaryDispatchContext.Provider value={{ onCreate, onDelete, onUpdate }}>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+
         </DiaryDispatchContext.Provider>
       </DiaryDataContext.Provider>
     </>
